ddb973258362638fcac35b3630d0a19a0b41316d
Adolfovi <32370254+Adolfovi@users.noreply.github.com>
2021-06-16 09:54:26 +0200

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,15 @@
+function isplata(){
+    document.getElementById('main-image').src="img/plata.jpg";
+
+
+
+}
+
+
+
+function isplomo(){
+    document.getElementById('main-image').src="img/plomo.jpg";
+
+
+
+}
